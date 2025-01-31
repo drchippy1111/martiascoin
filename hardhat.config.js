7fb4e26a78764d9c3161f4a14df50d8a497706b7
Uche <daniellerodriguez909009@gmail.com>
2025-01-31 18:22:30 +0100

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,9 +4,9 @@ require("dotenv").config();
 module.exports = {
     solidity: "0.8.0",
     networks: {
-        rinkeby: {
+        sepolia: {
             url: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
-            accounts: [`0x030242ea33a7da5af43d960bad${process.env.133cdb3cb8c2c86ce1655fc04579294b}`],
+            accounts: [`secret_passphrase 0xfe${process.env.133cdb3cb8c2c86ce1655fc04579294b}`],
         },
     },
 };
